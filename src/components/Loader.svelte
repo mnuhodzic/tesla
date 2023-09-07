82e1706dade797ff2ff31811cc41b69582659195
Muris Nuhodzic <muris.nuhodzic@blueface.com>
2023-09-07 16:58:41 +0200

    Loader center fix

--- src/components/Loader.svelte
+++ src/components/Loader.svelte
@@ -2,7 +2,7 @@
 	import { fade } from 'svelte/transition';
 </script>
 
-<div transition:fade class="lds-ellipsis absolute top-[40%] left-[45%]">
+<div transition:fade class="lds-ellipsis absolute top-[40%] left-[40%] sm:left-[45%] lg:left-[47%]">
 	<div class="bg-black dark:bg-white" />
 	<div class="bg-black dark:bg-white" />
 	<div class="bg-black dark:bg-white" />
